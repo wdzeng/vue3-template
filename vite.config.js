40a9fea3bd1c54929f3e4d3ad977d1b89b623b43
hyperbola <me@hyperbola.me>
2023-07-22 09:40:25 +0800

    chore: rename vite[st].config.js to .ts

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -18,7 +18,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:9487',
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: path => path.replace(/^\/api/, '')
       }
     }
   }
